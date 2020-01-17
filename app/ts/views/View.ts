e0c6898cfe4883ad934b5acea9294613c26236d4
pedroegerland <pedro.egerland94@gmail.com>
2020-01-16 23:29:57 -0200

    study overall part1

---
+++ app/ts/views/View.ts
@@ -0,0 +1,17 @@
+abstract class View<T> {
+
+    protected _elemento: JQuery;
+
+    constructor(selector: string){
+
+        this._elemento = $(selector);
+    }
+
+    update(model: T) {
+
+        this._elemento.html(this.template(model));
+    }
+
+    abstract template(model: T): string;
+
+}
